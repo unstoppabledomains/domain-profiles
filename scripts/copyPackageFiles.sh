1d9147e7e96b41304c94114845b58257cf347ba3
Aaron Quirk <aquirk@gmail.com>
2023-10-27 14:35:01 -0400

    NPM build verification in CI

--- scripts/copyPackageFiles.sh
+++ scripts/copyPackageFiles.sh
@@ -1,11 +1,11 @@
 # Prepare the UI components package
-cd packages/ui-components
-cp -rf CHANGELOG.md ../../README.md ../../LICENSE build/src
-cat package.json | sed -e 's/build\/src\///g' | sed -e 's/workspace:\^/latest/g' | sed -e 's/"private": true/"private": false/g' > build/src/package.json
-cd ../
-
-# Prepare the config package
-cd packages/config
-cp -rf CHANGELOG.md ../../README.md ../../LICENSE build/src
-cat package.json | sed -e 's/build\/src\///g' | sed -e 's/workspace:\^/latest/g' | sed -e 's/"private": true/"private": false/g' > build/src/package.json
-cd ../
+cd packages/ui-components \
+  && cp -rf CHANGELOG.md ../../README.md ../../LICENSE build/src \
+  && cat package.json | sed -e 's/build\/src\///g' | sed -e 's/workspace:\^/latest/g' | sed -e 's/"private": true/"private": false/g' > build/src/package.json \
+  && cd ../../ \
+  &&
+  # Prepare the config package
+  cd packages/config \
+  && cp -rf CHANGELOG.md ../../README.md ../../LICENSE build/src \
+  && cat package.json | sed -e 's/build\/src\///g' | sed -e 's/workspace:\^/latest/g' | sed -e 's/"private": true/"private": false/g' > build/src/package.json \
+  && cd ../../

12d046476f3e77aa95966adf46df14580e62def3
Aaron Quirk <aquirk@gmail.com>
2023-10-27 13:44:47 -0400

    Update dist scripts

--- scripts/copyPackageFiles.sh
+++ scripts/copyPackageFiles.sh
@@ -1,11 +1,11 @@
 # Prepare the UI components package
 pushd packages/ui-components
 cp -rf CHANGELOG.md ../../README.md ../../LICENSE build/src
-cat package.json | sed -e 's/build\/src\///g' | sed -e 's/workspace:\^/latest/g' > build/src/package.json
+cat package.json | sed -e 's/build\/src\///g' | sed -e 's/workspace:\^/latest/g' | sed -e 's/"private": true/"private": false/g' > build/src/package.json
 popd
 
 # Prepare the config package
 pushd packages/config
 cp -rf CHANGELOG.md ../../README.md ../../LICENSE build/src
-cat package.json | sed -e 's/build\/src\///g' | sed -e 's/workspace:\^/latest/g' > build/src/package.json
+cat package.json | sed -e 's/build\/src\///g' | sed -e 's/workspace:\^/latest/g' | sed -e 's/"private": true/"private": false/g' > build/src/package.json
 popd

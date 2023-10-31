f00c1db5037dbb297c2c6122024e159a0f08166f
Aaron Quirk <aquirk@gmail.com>
2023-10-31 08:52:07 -0400

    Update build command

--- scripts/copyPackageFiles.sh
+++ scripts/copyPackageFiles.sh
@@ -1,4 +1,5 @@
 # Prepare the UI components package
+echo "Copying files for NPM packages..."
 cd packages/ui-components \
   && cp -rf CHANGELOG.md ../../README.md ../../LICENSE build/src \
   && cat package.json | sed -e 's/build\/src\///g' | sed -e 's/workspace:\^/latest/g' | sed -e 's/"private": true/"private": false/g' > build/src/package.json \

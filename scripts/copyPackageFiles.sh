872e4beaaa211f646a761d994c1e3d101919c657
Yevhenii Osadchyi <yevhenii@unstoppabledomains.com>
2024-07-22 23:54:02 +0100

    Fix copyPackageFiles.sh to work with node 18

--- scripts/copyPackageFiles.sh
+++ scripts/copyPackageFiles.sh
@@ -1,12 +1,26 @@
+#!/bin/bash
+
+# Ensure the script exits on any command failure
+set -e
+
+# Function to prepare an NPM package
+prepare_package() {
+  local package_dir=$1
+
+  echo "Copying files for ${package_dir}..."
+
+  cd $package_dir \
+    && cp -rf CHANGELOG.md ../../README.md ../../LICENSE build/src \
+    && cat package.json | sed -e 's/build\/src\///g' \
+      -e 's/workspace:\^/latest/g' \
+      -e 's/"private": true/"private": false/g' > build/src/package.json \
+    && cd ../../
+
+  echo "Done."
+}
+
 # Prepare the UI components package
-echo "Copying files for NPM packages..."
-cd packages/ui-components \
-  && cp -rf CHANGELOG.md ../../README.md ../../LICENSE build/src \
-  && cat package.json | sed -e 's/build\/src\///g' | sed -e 's/workspace:\^/latest/g' | sed -e 's/"private": true/"private": false/g' > build/src/package.json \
-  && cd ../../ \
-  &&
-  # Prepare the config package
-  cd packages/config \
-  && cp -rf CHANGELOG.md README.md ../../LICENSE build/src \
-  && cat package.json | sed -e 's/build\/src\///g' | sed -e 's/workspace:\^/latest/g' | sed -e 's/"private": true/"private": false/g' > build/src/package.json \
-  && cd ../../
+prepare_package "packages/ui-components"
+
+# Prepare the config package
+prepare_package "packages/config"

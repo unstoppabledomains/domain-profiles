56c789996a2563446b890d6ca3e92868f81d4be8
Aaron Quirk <aquirk@gmail.com>
2024-08-27 14:05:45 -0400

    Support formatted typed v4 messages

--- packages/ui-components/src/components/Wallet/index.tsx
+++ packages/ui-components/src/components/Wallet/index.tsx
@@ -4,4 +4,5 @@ export * from './DomainWalletTransactions';
 export * from './SignForDappHeader';
 export * from './TitleWithBackButton';
 export * from './TokensPortfolio';
+export * from './TypedMessage';
 export * from './Wallet';

dd4eccda8cc1de8c497beb51185ef1a9a52ce8f5
Aaron Quirk <aaron.quirk@unstoppabledomains.com>
2024-11-02 12:17:59 -0400

    Revert "Revert "Support send of arbitrary ERC-20 tokens"" (#225)
    * Revert "Revert "Support send of arbitrary ERC-20 tokens (#223)" (#224)"
    
    This reverts commit be327f421f3d1df8038cd3363651001df3509e50.
    
    * handle erc-20 address validation
    
    * bump package versions

--- packages/ui-components/src/components/Wallet/Send.tsx
+++ packages/ui-components/src/components/Wallet/Send.tsx
@@ -11,14 +11,17 @@ import {makeStyles} from '@unstoppabledomains/ui-kit/styles';
 import {useFeatureFlags} from '../../actions';
 import {
   getAccountAssets,
+  getTransactionGasEstimate,
   getTransferGasEstimate,
 } from '../../actions/fireBlocksActions';
 import {prepareRecipientWallet} from '../../actions/walletActions';
 import type {SerializedWalletBalance} from '../../lib';
-import {useTranslationContext} from '../../lib';
+import {TokenType, useTranslationContext} from '../../lib';
 import {sleep} from '../../lib/sleep';
 import type {AccountAsset} from '../../lib/types/fireBlocks';
 import {getAsset} from '../../lib/wallet/asset';
+import {getProviderUrl} from '../../lib/wallet/evm/provider';
+import {createErc20TransferTx} from '../../lib/wallet/evm/token';
 import {isEthAddress} from '../Chat/protocol/resolution';
 import {getBlockchainDisplaySymbol} from '../Manage/common/verification/types';
 import AddressInput from './AddressInput';
@@ -212,16 +215,41 @@ const Send: React.FC<Props> = ({
       throw new Error('Asset not found');
     }
 
-    // estimate the gas cost
-    const gasResponse = await getTransferGasEstimate(
-      assetToSend,
-      accessToken,
-      // Doesn't matter what the recipient and amount are, just need to get the fee estimate
-      assetToSend.address,
-      // Use a small test amount to measure gas
-      '0.0001',
-    );
-    setGasFeeEstimate(gasResponse.networkFee?.amount || '0');
+    // depending on the type of token, estimate the required gas
+    if (
+      assetToSend.blockchainAsset.blockchain.networkId &&
+      token.type === TokenType.Erc20 &&
+      token.address
+    ) {
+      // retrieve gas for a transaction
+      const transferTx = await createErc20TransferTx({
+        chainId: assetToSend.blockchainAsset.blockchain.networkId,
+        providerUrl: getProviderUrl(assetToSend.blockchainAsset.blockchain.id),
+        tokenAddress: token.address,
+        fromAddress: token.walletAddress,
+        toAddress: token.walletAddress,
+        amount: 0.000001,
+      });
+      const transferTxGas = await getTransactionGasEstimate(
+        assetToSend,
+        accessToken,
+        transferTx,
+      );
+      setGasFeeEstimate(transferTxGas.networkFee?.amount || '0');
+    } else {
+      // retrieve gas for a transfer
+      const transferGas = await getTransferGasEstimate(
+        assetToSend,
+        accessToken,
+        // Doesn't matter what the recipient and amount are, just need to get the fee estimate
+        assetToSend.address,
+        // Use a small test amount to measure gas
+        '0.0001',
+      );
+      setGasFeeEstimate(transferGas.networkFee?.amount || '0');
+    }
+
+    // save the asset entry to be sent
     setAccountAsset(assetToSend);
     setIsLoading(false);
   };
@@ -280,11 +308,13 @@ const Send: React.FC<Props> = ({
     }
 
     // normalize asset decimals if present
-    const normalizedValue = accountAsset.balance?.decimals
-      ? `0.${value
-          .replaceAll('0.', '')
-          .slice(0, accountAsset.balance.decimals)}`
-      : value;
+    const normalizedBase = parseInt(value, 10);
+    const normalizedValue =
+      value.includes('.') && accountAsset.balance?.decimals
+        ? `${normalizedBase}.${value
+            .replaceAll(`${normalizedBase}.`, '')
+            .slice(0, accountAsset.balance.decimals)}`
+        : value;
 
     // use normalized value
     setAmount(normalizedValue);
@@ -314,7 +344,8 @@ const Send: React.FC<Props> = ({
           onClickReceive={onClickReceive}
           label={t('wallet.selectAssetToSend')}
           requireBalance={true}
-          supportedTokenList={config.WALLETS.CHAINS.SEND}
+          supportedAssetList={config.WALLETS.CHAINS.SEND}
+          supportErc20={true}
         />
       </Box>
     );
@@ -326,6 +357,7 @@ const Send: React.FC<Props> = ({
         <SendConfirm
           gasFee={gasFeeEstimate}
           asset={accountAsset}
+          token={selectedToken}
           onBackClick={handleBackClick}
           onSendClick={handleSubmitTransaction}
           recipientAddress={recipientAddress}
@@ -357,6 +389,7 @@ const Send: React.FC<Props> = ({
           onInvitation={handleSendInvitation}
           accessToken={accessToken}
           asset={accountAsset}
+          token={selectedToken}
           recipientAddress={recipientAddress}
           recipientDomain={resolvedDomain}
           amount={amount}
@@ -366,12 +399,24 @@ const Send: React.FC<Props> = ({
     );
   }
 
-  const insufficientBalance = parseFloat(amount) > selectedToken.balance;
+  // determine how much balance for gas token
+  const gasTokenBalance =
+    wallets.find(
+      w =>
+        w.address.toLowerCase() === selectedToken.walletAddress.toLowerCase() &&
+        w.symbol.toLowerCase() === selectedToken.symbol.toLowerCase(),
+    )?.balanceAmt || 0;
+
+  // determine insufficient gas or token balance
+  const insufficientBalance =
+    parseFloat(amount) > selectedToken.balance ||
+    parseFloat(gasFeeEstimate || '0') > gasTokenBalance;
 
   const canSend = Boolean(
     !insufficientBalance &&
       recipientAddress &&
       !transactionSubmitted &&
+      gasTokenBalance !== 0 &&
       parseFloat(amount) !== 0 &&
       !isNaN(parseFloat(amount)),
   );
@@ -409,7 +454,11 @@ const Send: React.FC<Props> = ({
             />
           </Box>
           <AmountInput
-            gasFeeEstimate={gasFeeEstimate}
+            gasFeeEstimate={
+              selectedToken.symbol === selectedToken.ticker
+                ? gasFeeEstimate
+                : '0'
+            }
             amountInputRef={amountInputRef}
             token={selectedToken}
             initialAmount={amount}
---
+++ packages/ui-components/src/lib/wallet/evm/token.test.ts
@@ -0,0 +1,27 @@
+import {createErc20TransferTx} from './token';
+import * as web3 from './web3';
+
+describe('token transactions', () => {
+  it('should create an erc20 transfer tx', async () => {
+    // mock the contract decimals
+    jest.spyOn(web3, 'getContractDecimals').mockResolvedValue(6);
+
+    // create the transaction
+    const tx = await createErc20TransferTx({
+      chainId: 80002,
+      providerUrl: 'https://mock-provider-url',
+      tokenAddress: '0x41e94eb019c0762f9bfcf9fb1e58725bfb0e7582',
+      fromAddress: '0xCD0DAdAb45bAF9a06ce1279D1342EcC3F44845af',
+      toAddress: '0x8ee1E1d88EBE2B44eAD162777DE787Ef6A2dC2F2',
+      amount: 0.000001,
+    });
+    expect(tx).toMatchObject({
+      chainId: 80002,
+      to: '0x41e94eb019c0762f9bfcf9fb1e58725bfb0e7582',
+      // expected data generated directly from Polygon scan using MetaMask:
+      // https://amoy.polygonscan.com/token/0x41e94eb019c0762f9bfcf9fb1e58725bfb0e7582?a=0x8ee1e1d88ebe2b44ead162777de787ef6a2dc2f2#writeProxyContract
+      data: '0xa9059cbb0000000000000000000000008ee1e1d88ebe2b44ead162777de787ef6a2dc2f20000000000000000000000000000000000000000000000000000000000000001',
+      value: '0',
+    });
+  });
+});

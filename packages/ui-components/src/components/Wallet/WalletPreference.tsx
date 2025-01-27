360e9f80cce2547b77875bd4b2be2487627aa1b0
Aaron Quirk <aquirk@gmail.com>
2025-01-27 13:40:34 -0500

    refine for extension usage

--- packages/ui-components/src/components/Wallet/WalletPreference.tsx
+++ packages/ui-components/src/components/Wallet/WalletPreference.tsx
@@ -5,31 +5,39 @@ import AccordionSummary from '@mui/material/AccordionSummary';
 import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
 import {useTheme} from '@mui/material/styles';
-import React from 'react';
+import React, {useState} from 'react';
 
 export const WalletPreference: React.FC<WalletPreferenceProps> = ({
   title,
   statusElement,
   description,
   icon,
-  expanded,
+  expanded: forceExpanded,
   children,
 }) => {
   const theme = useTheme();
+  const [expanded, setExpanded] = useState<boolean>();
+
+  function handleClick(): void {
+    setExpanded(!expanded);
+  }
 
   return (
     <Accordion
       sx={{
-        backgroundColor: expanded ? theme.palette.dangerShades[100] : undefined,
-        color: expanded
+        backgroundColor: forceExpanded
+          ? theme.palette.dangerShades[100]
+          : undefined,
+        color: forceExpanded
           ? theme.palette.getContrastText(theme.palette.dangerShades[100])
           : undefined,
       }}
-      expanded={expanded ? true : undefined}
+      expanded={forceExpanded ? true : expanded}
     >
       <AccordionSummary
+        onClick={forceExpanded ? undefined : handleClick}
         expandIcon={
-          <ExpandMoreIcon sx={{display: expanded ? 'none' : undefined}} />
+          <ExpandMoreIcon sx={{display: forceExpanded ? 'none' : undefined}} />
         }
       >
         <Box
@@ -41,13 +49,13 @@ export const WalletPreference: React.FC<WalletPreferenceProps> = ({
           <Box display="flex" alignItems="center">
             {icon}
             <Typography
-              variant={expanded ? 'subtitle1' : 'subtitle2'}
+              variant={forceExpanded ? 'subtitle1' : 'subtitle2'}
               fontWeight="bold"
             >
               {title}
             </Typography>
           </Box>
-          {statusElement && (
+          {statusElement && !expanded && (
             <Box display="flex" alignItems="center" mr={1}>
               {statusElement}
             </Box>

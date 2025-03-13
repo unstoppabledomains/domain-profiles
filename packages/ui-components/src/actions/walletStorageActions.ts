b8f44953856d81f772cacf1402343387e0f0ae1c
Aaron Quirk <aquirk@gmail.com>
2025-03-13 10:28:28 -0400

    updates from comments

--- packages/ui-components/src/actions/walletStorageActions.ts
+++ packages/ui-components/src/actions/walletStorageActions.ts
@@ -1,43 +1,48 @@
+import {Mutex} from 'async-mutex';
+
 import config from '@unstoppabledomains/config';
 
 import {fetchApi} from '../lib/fetchApi';
 import type {WalletStorageData} from '../lib/types/walletStorage';
 import {getAccountId} from './fireBlocksActions';
 
+// wallet storage control variables
 const WALLET_KEY_NAME = 'general-preferences';
+const walletStorageMutex = new Mutex();
 
 export const clearWalletStorageData = async (
   accessToken: string,
   accountId?: string,
-) => {
-  try {
-    // retrieve account ID if empty
-    const normalizedAccountId =
-      accountId ?? (await getAccountId(accessToken, true));
-    if (!normalizedAccountId) {
-      return undefined;
-    }
+): Promise<WalletStorageData | undefined> => {
+  return await walletStorageMutex.runExclusive(async () => {
+    try {
+      // retrieve account ID if empty
+      const normalizedAccountId =
+        accountId ?? (await getAccountId(accessToken, true));
+      if (!normalizedAccountId) {
+        return undefined;
+      }
 
-    // remove data from storage
-    const response = await fetchApi<WalletStorageData>(
-      `/user/${normalizedAccountId}/wallet/storage/${WALLET_KEY_NAME}`,
-      {
-        host: config.PROFILE.HOST_URL,
-        method: 'DELETE',
-        headers: {
-          'Content-type': 'application/json',
-          Authorization: `Bearer ${accessToken}`,
+      // remove data from storage
+      const response = await fetchApi<WalletStorageData>(
+        `/user/${normalizedAccountId}/wallet/storage/${WALLET_KEY_NAME}`,
+        {
+          host: config.PROFILE.HOST_URL,
+          method: 'DELETE',
+          headers: {
+            Authorization: `Bearer ${accessToken}`,
+          },
         },
-      },
-    );
+      );
 
-    // update cache and return
-    if (response) {
-      sessionStorage.removeItem(WALLET_KEY_NAME);
-      return response;
-    }
-  } catch (e) {}
-  return undefined;
+      // update cache and return
+      if (response) {
+        sessionStorage.removeItem(WALLET_KEY_NAME);
+        return response;
+      }
+    } catch (e) {}
+    return undefined;
+  });
 };
 
 export const getWalletStorageData = async (
@@ -45,77 +50,79 @@ export const getWalletStorageData = async (
   accountId?: string,
   forceRefresh = false,
 ): Promise<WalletStorageData | undefined> => {
-  try {
-    // retrieve from session storage if available
-    const data = sessionStorage.getItem(WALLET_KEY_NAME);
-    if (data && !forceRefresh) {
-      return JSON.parse(data);
-    }
+  return await walletStorageMutex.runExclusive(async () => {
+    try {
+      // retrieve from session storage if available
+      const data = sessionStorage.getItem(WALLET_KEY_NAME);
+      if (data && !forceRefresh) {
+        return JSON.parse(data);
+      }
 
-    // retrieve account ID if empty
-    const normalizedAccountId =
-      accountId ?? (await getAccountId(accessToken, true));
-    if (!normalizedAccountId) {
-      return undefined;
-    }
+      // retrieve account ID if empty
+      const normalizedAccountId =
+        accountId ?? (await getAccountId(accessToken, true));
+      if (!normalizedAccountId) {
+        return undefined;
+      }
 
-    // retrieve new data if not available in session storage
-    const response = await fetchApi<WalletStorageData>(
-      `/user/${normalizedAccountId}/wallet/storage/${WALLET_KEY_NAME}`,
-      {
-        host: config.PROFILE.HOST_URL,
-        method: 'GET',
-        headers: {
-          'Content-type': 'application/json',
-          Authorization: `Bearer ${accessToken}`,
+      // retrieve new data if not available in session storage
+      const response = await fetchApi<WalletStorageData>(
+        `/user/${normalizedAccountId}/wallet/storage/${WALLET_KEY_NAME}`,
+        {
+          host: config.PROFILE.HOST_URL,
+          method: 'GET',
+          headers: {
+            Authorization: `Bearer ${accessToken}`,
+          },
         },
-      },
-    );
+      );
 
-    // update cache and return
-    sessionStorage.setItem(WALLET_KEY_NAME, JSON.stringify(response || {}));
-    if (response) {
-      return response;
+      // update cache and return
+      sessionStorage.setItem(WALLET_KEY_NAME, JSON.stringify(response || {}));
+      if (response) {
+        return response;
+      }
+    } catch (e) {
+      // set empty object in session storage
+      sessionStorage.setItem(WALLET_KEY_NAME, JSON.stringify({}));
     }
-  } catch (e) {
-    // set empty object in session storage
-    sessionStorage.setItem(WALLET_KEY_NAME, JSON.stringify({}));
-  }
-  return undefined;
+    return undefined;
+  });
 };
 
 export const setWalletStorageData = async (
   data: Record<string, string>,
   accessToken: string,
   accountId?: string,
-) => {
-  try {
-    // retrieve account ID if empty
-    const normalizedAccountId =
-      accountId ?? (await getAccountId(accessToken, true));
-    if (!normalizedAccountId) {
-      return undefined;
-    }
+): Promise<WalletStorageData | undefined> => {
+  return await walletStorageMutex.runExclusive(async () => {
+    try {
+      // retrieve account ID if empty
+      const normalizedAccountId =
+        accountId ?? (await getAccountId(accessToken, true));
+      if (!normalizedAccountId) {
+        return undefined;
+      }
 
-    // set the wallet storage data
-    const response = await fetchApi<WalletStorageData>(
-      `/user/${normalizedAccountId}/wallet/storage/${WALLET_KEY_NAME}`,
-      {
-        host: config.PROFILE.HOST_URL,
-        method: 'POST',
-        headers: {
-          'Content-type': 'application/json',
-          Authorization: `Bearer ${accessToken}`,
+      // set the wallet storage data
+      const response = await fetchApi<WalletStorageData>(
+        `/user/${normalizedAccountId}/wallet/storage/${WALLET_KEY_NAME}`,
+        {
+          host: config.PROFILE.HOST_URL,
+          method: 'POST',
+          headers: {
+            Authorization: `Bearer ${accessToken}`,
+          },
+          body: JSON.stringify(data),
         },
-        body: JSON.stringify(JSON.stringify(data)),
-      },
-    );
+      );
 
-    // update cache and return
-    if (response) {
-      sessionStorage.setItem(WALLET_KEY_NAME, JSON.stringify(response));
-      return response;
-    }
-  } catch (e) {}
-  return undefined;
+      // update cache and return
+      if (response) {
+        sessionStorage.setItem(WALLET_KEY_NAME, JSON.stringify(response));
+        return response;
+      }
+    } catch (e) {}
+    return undefined;
+  });
 };
